ddd2373cd8db08c9dd4ef362db6699f268429987
Leotor1157 <leo.tor1157@gmail.com>
2023-02-08 16:54:00 +0100

    fix search.js

--- src/search.js
+++ src/search.js
@@ -18,33 +18,20 @@ export async function searchForCocktails() {
 }
 
 export function createCocktail(nom, image) {
-    // On créer la div principal
     const root = document.createElement('div')
     root.classList.add('cocktail')
-  
-    // On créer la div contenant l'image
     const imgContainer = document.createElement('div')
     imgContainer.classList.add('img-container')
-  
-    // On créer la div pour le detail
     const detail = document.createElement('div')
     detail.classList.add('detail')
-  
-    // On créé l'image
     const img = document.createElement('img')
     img.setAttribute('alt', nom)
     img.setAttribute('src', image)
-  
-    // On crée le paragraph contenant le nom du cocktail
     const p = document.createElement('p')
     p.innerText = nom
-  
-    // On assemble les éléments
     root.append(imgContainer)
     root.append(detail)
     imgContainer.append(img)
     detail.append(p)
-  
-    // On retourne la div
     return root
   }
